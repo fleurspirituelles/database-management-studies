296e8cc587a8cf5870242b0b53cfdde2e49ba496
talitha <talitha.vieira@aluno.ifsp.edu.br>
2024-07-29 09:10:00 -0300

    feat: create table

--- system.sql
+++ system.sql
@@ -1,3 +1,22 @@
 /* A cada início de sessão: */
-ALTER SESSION SET "_ORACLE_SCRIPT" = TRUE;
+ALTER SESSION SET "_ORACLE_SCRIPT" = true;
 /* Habilita a execução de scripts com permissões de sistema. */
+
+CREATE TABLE atleta (
+    id       NUMBER(4) NOT NULL,
+    cpf      VARCHAR2(14) NOT NULL,
+    nome     VARCHAR2(60) NOT NULL,
+    sexo     CHAR(1),
+    datanasc DATE,
+    endereco VARCHAR2(50),
+    salario  NUMBER(8, 2) NOT NULL,
+    id_clube NUMBER(4),
+    CONSTRAINT atleta_pk PRIMARY KEY ( id ),
+    CONSTRAINT atleta_uk UNIQUE ( cpf ),
+    CONSTRAINT atleta_sexo_ck CHECK ( sexo IN ( 'M', 'F', 'm', 'f' ) ),
+    CONSTRAINT atleta_salario_ck CHECK ( salario > 0 ),
+    CONSTRAINT atleta_clube_fk FOREIGN KEY ( id_clube )
+        REFERENCES clube ( id )
+);
+
+/* Como a tabela clube ainda não existe, criamos sem a foreign key referenciando clube. */
