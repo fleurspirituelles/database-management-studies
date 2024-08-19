3f83333b971b345484e0be9dd658f54dd7df3cc6
talitha <talitha.vieira@aluno.ifsp.edu.br>
2024-08-19 08:56:05 -0300

    feat: add new users, roles, and privileges

--- snoopy.sql
+++ snoopy.sql
@@ -6,11 +6,30 @@ CREATE TABLE teste_priv (
 ALTER TABLE teste_priv MODIFY
     texto VARCHAR(20);
 
-DROP TABLE teste_priv;
-
 INSERT INTO teste_priv VALUES (
     1,
     'this is a text'
 );
 
 CREATE SEQUENCE teste_id_seq INCREMENT BY 1 START WITH 2 NOCACHE;
+
+INSERT INTO teste_priv VALUES (
+    teste_id_seq.NEXTVAL,
+    'another text'
+);
+
+CREATE VIEW v_teste_priv AS
+    SELECT
+        texto
+    FROM
+        teste_priv;
+
+CREATE TABLE woodstock.teste_priv2 (
+    id        NUMBER(1),
+    descricao CHAR(1)
+);
+
+INSERT INTO woodstock.teste_priv2 VALUES (
+    5,
+    'x'
+);
