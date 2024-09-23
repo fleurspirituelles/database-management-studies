be9f7c3ca7966565be2a66b3bda781176aebfec0
talitha <talitha.vieira@aluno.ifsp.edu.br>
2024-09-23 08:27:32 -0300

    feat: conditional logic

--- plsql.sql
+++ plsql.sql
@@ -28,3 +28,20 @@ BEGIN
     dbms_output.put_line(v_message);
 END;
 
+DECLARE
+    estado_civil CHAR(1) := 'D';
+    v_estcivil   VARCHAR(20);
+BEGIN
+    CASE estado_civil
+        WHEN 'C' THEN
+            v_estcivil := 'Casada';
+        WHEN 'S' THEN
+            v_estcivil := 'Solteira';
+        WHEN 'D' THEN
+            v_estcivil := 'Divorciada';
+        ELSE
+            v_estcivil := 'Não informado';
+    END CASE;
+
+    dbms_output.put_line('Estado civil: ' || v_estcivil);
+END;
